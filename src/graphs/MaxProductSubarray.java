754aec96ce5ecba264c1afd5f5347c7524e3a601
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-08-01 23:43:38 +0530

    GRAPHS: 18. Maximum product subarray

---
+++ src/graphs/MaxProductSubarray.java
@@ -0,0 +1,18 @@
+class MaxProductSubarray {
+    public int maxProduct(int[] nums) {
+        int left = 1; int right = 1; int ans = Integer. MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            if(left == 0){
+                left =1;
+            }
+            if(right == 0){
+                right =1;
+            }
+            left *= nums[i];
+            right *= nums[nums.length-1-i];
+            ans = Math.max(ans,Math.max(left,right));
+            
+        }
+        return ans;
+    }
+}
