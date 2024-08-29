f91f188fb2bd2a9a7e923924edd387881cfffc51
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-08-29 22:00:36 +0530

    ARRAYS: 36. Sub array sum equals K

---
+++ src/arrays/SubArraySumEqualsK.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.HashMap;
+
+public class SubArraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> prefixSum = new HashMap<>();
+        prefixSum.put(0, 1);
+        int currsum = 0;
+        int removeSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currsum += nums[i];
+            removeSum = currsum - k;
+
+            if (prefixSum.containsKey(removeSum)) {
+                count += prefixSum.get(removeSum);
+            }
+            prefixSum.put(currsum, prefixSum.getOrDefault(currsum, 0) + 1);
+        }
+        return count;
+    }
+}
